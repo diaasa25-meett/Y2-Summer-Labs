c2d0a6dad80650a003c82b13d5c63fda324595b4
diaasa25-meett <diaasaad2008@gmail.com>
2024-07-16 00:13:11 +0300

    Create pythonReview.py

---
+++ 2.1 python review #1/pythonReview.py
@@ -0,0 +1,5 @@
+def create_youtube_video():
+	title=input ("enter a title for the youtube video")
+	description=input("enter a description for the youtube video")
+	veddic={"tilte":title,"description":description
+	}
